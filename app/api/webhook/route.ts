48ca446a3e1607a825b42a6468fbd321b1457438
Biplob Kumar Sutradhar <biplob.caretutors@gmail.com>
2024-11-30 16:47:36 +0600

    update

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,10 +1,10 @@
 
 import { helloWorldTask } from "@/src/trigger/example";
 import { tasks } from "@trigger.dev/sdk/v3";
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextApiRequest) {
     // Parse the webhook payload
     const payload = req.body;
 
